4966e0e69cbb0aae58c023c86ce235424c54f9c3
Seyyed Arya Hassanli <31996976+AryaHassanli@users.noreply.github.com>
2020-10-18 20:04:39 +0200

    Clean Main & AgeDB added

--- config.py
+++ config.py
@@ -4,36 +4,35 @@ import torch
 
 
 class Config:
-    """
-        An object for the main configs
-
-        Attributes:
-            baseDir: The base working directory. Should be the directory which main.py is located. 
-                Default is Current Directory
-            datasetDir: The directory which includes Datasets. Absolute and Relative paths are accepted.
-                Default is "datasets"
-            absDataset: The absolute dataset directory generated from baseDir and datasetDir
-        
-        Methods:
-
-    """
-
     def __init__(self):
         self.baseDir = os.path.dirname(os.path.realpath(__file__))
+
         self.datasetDir = "datasets"
-        self.absDatasetDir = os.path.join(self.baseDir, self.datasetDir)
+        self.absDatasetDir = self.datasetDir if os.path.isabs(self.datasetDir) else os.path.join(self.baseDir,
+                                                                                                 self.datasetDir)
+        self.outputDir = "output"
+        self.absOutputDir = self.outputDir if os.path.isabs(self.outputDir) else os.path.join(self.baseDir,
+                                                                                              self.outputDir)
+
         self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
         pass
 
     def set(self,
             baseDir=None,
             datasetDir=None,
+            outputDir=None,
             device=None
             ):
         self.baseDir = baseDir if baseDir is not None else self.baseDir
+
         self.datasetDir = datasetDir if datasetDir is not None else self.datasetDir
+        self.absDatasetDir = self.datasetDir if os.path.isabs(self.datasetDir) else os.path.join(self.baseDir,
+                                                                                                 self.datasetDir)
+        self.outputDir = outputDir if outputDir is not None else self.outputDir
+        self.absOutputDir = self.outputDir if os.path.isabs(self.outputDir) else os.path.join(self.baseDir,
+                                                                                              self.outputDir)
+
         self.device = device if device is not None else self.device
-        self.absDatasetDir = os.path.join(self.baseDir, self.datasetDir)
 
 
 config = Config()
