00e0f09bd08a7ea22387b105e9842f7c988912df
Seyyed Arya Hassanli <31996976+AryaHassanli@users.noreply.github.com>
2020-10-23 18:03:33 +0200

    net and loader automation

---
+++ dataLoaders/__init__.py
@@ -0,0 +1,5 @@
+import os
+from config import config
+dir = os.path.join(config.baseDir, 'dataLoaders')
+__all__ = [f[:-3] for f in os.listdir(dir) if os.path.isfile(os.path.join(dir,f)) and not f.endswith('__init__.py')]
+print(__all__)
