c6cdf1f4b1eafe321bc983d53f9799b8e63d3d4b
Seyyed Arya Hassanli <31996976+AryaHassanli@users.noreply.github.com>
2020-10-24 21:51:10 +0200

    get transforms as an argument

--- helpers/getLoaders.py
+++ helpers/getLoaders.py
@@ -1,13 +1,9 @@
-import torchvision.transforms as transforms
 # noinspection PyUnresolvedReferences
 from dataLoaders import *
 
 
-def getLoaders(dataset, feature, splitSize, batchSize):
+def getLoaders(dataset, feature, splitSize, batchSize, transform):
     dataObj = eval(dataset+"."+dataset+'Class(feature="' + feature + '")')
-    dataObj.createDataset(transform=transforms.Compose(
-            [transforms.Resize((60, 60)),
-             transforms.ToTensor(),
-             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])]))
+    dataObj.createDataset(transform=transform)
     dataObj.splitDataset(trainSize=splitSize[0], validateSize=splitSize[1], testSize=splitSize[2])
     return dataObj.dataLoaders(batchSize=batchSize)
