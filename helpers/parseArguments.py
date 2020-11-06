e775f53f5de58cbff080e2540371af35507b012b
Seyyed Arya Hassanli <31996976+AryaHassanli@users.noreply.github.com>
2020-11-06 10:07:24 +0100

    Added: Load defaults from options/*.json

--- helpers/parseArguments.py
+++ helpers/parseArguments.py
@@ -1,4 +1,8 @@
 import argparse
+import json
+
+with open('options/train.json') as json_file:
+    trainOptions = json.load(json_file)
 
 
 def parseArguments(function):
@@ -27,27 +31,36 @@ def parseArguments(function):
     if function == 'train':
         parser.add_argument('-G', '--gradient',
                             action='store_true',
-                            help='to run on gradient')
+                            help='to run on gradient',
+                            default=trainOptions['gradient']
+                            )
         parser.add_argument('--dataset',
                             metavar='dataset',
                             type=str,
                             choices=['UTKFace', 'AgeDB'],
                             help='select the dataset: %(choices)s',
-                            default='AgeDB'
+                            default=trainOptions['dataset']
                             )
         parser.add_argument('--net',
                             metavar='net',
                             type=str,
                             choices=['resnet18', 'resnet50', 'mobilenet_v2', 'mobilenet_v3'],
                             help='select the net: %(choices)s',
-                            default='mobilenet_v2'
+                            default=trainOptions['net']
+                            )
+        parser.add_argument('--task',
+                            metavar='task',
+                            type=str,
+                            choices=['classification', 'regression'],
+                            help='select the task type: %(choices)s',
+                            default=trainOptions['task']
                             )
         parser.add_argument('--feature',
                             metavar='feature',
                             type=str,
                             choices=['age', 'gender'],
                             help='select the feature: %(choices)s',
-                            default='gender'
+                            default=trainOptions['feature']
                             )
 
         parser.add_argument('--splitSize',
@@ -55,19 +68,25 @@ def parseArguments(function):
                             nargs=3,
                             type=float,
                             help='specify the train, validation and test size',
-                            default=[0.7, 0.2, 0.1]
+                            default=trainOptions['splitSize']
                             )
         parser.add_argument('--batchSize',
                             metavar='batchSize',
                             type=int,
                             help='set the batch size',
-                            default=15
+                            default=trainOptions['batchSize']
                             )
         parser.add_argument('--epochs',
                             metavar='num of epochs',
                             type=int,
                             help='set the number of epochs',
-                            default=10
+                            default=trainOptions['epochs']
                             )
+        parser.add_argument('--tag',
+                            metavar='tag',
+                            help='add a tag at the end of the file',
+                            default=''
+                            )
+
     args = parser.parse_args()
     return args
