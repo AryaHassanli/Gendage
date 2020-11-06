2c6b0f7a8899bdcc0f0bb236b90da9ad022b7b84
Seyyed Arya Hassanli <31996976+AryaHassanli@users.noreply.github.com>
2020-11-06 10:08:31 +0100

    Commit

--- helpers/eval.py
+++ helpers/eval.py
@@ -7,15 +7,9 @@ from PIL import Image
 from config import config
 from helpers.getNet import getNet
 
-genderModel = getNet('resnet18')
-genderModel.load_state_dict(
-    torch.load('D:/MsThesis/models/resnet18_AgeDB_gender_model/resnet18_AgeDB_gender_model.pt'))
-genderModel.eval()
+genderModel = None
+ageModel = None
 
-ageModel = getNet('resnet50')
-ageModel.load_state_dict(
-    torch.load('D:/MsThesis/1-Age/AgeClasses_resnet50_AgeDB/resnet50_AgeDB_age_model.pt'))
-ageModel.eval()
 ageLabels = {0: '0-2',
              1: '2-5',
              2: '5-9',
@@ -32,10 +26,32 @@ totalError = 0
 totalTime = 0
 
 
-def genderAndAge(face):
+def gendageV1(face):
+    """
+    Two separate Models
+    Gender: Classification. resnet18
+    Age:    Classification on Age Groups. resnet50
+    Args:
+        face:
+
+    Returns:
+
+    """
+    global genderModel, ageModel
+    if genderModel is None:
+        genderModel = getNet('resnet18')
+        genderModel.load_state_dict(
+            torch.load('D:/MsThesis/0-gender/resnet18_AgeDB_gender_model/resnet18_AgeDB_gender_model.pt'))
+        genderModel.eval()
+    if ageModel is None:
+        ageModel = getNet('resnet50')
+        ageModel.load_state_dict(
+            torch.load('D:/MsThesis/1-age/AgeClasses_resnet50_AgeDB/resnet50_AgeDB_age_model.pt'))
+        ageModel.eval()
+
     global totalError, totalFaces, totalTime, ageLabels
     transform = transforms.Compose(
-        [transforms.Resize((100, 100)),
+        [transforms.Resize((60, 60)),
          transforms.ToTensor(),
          transforms.Normalize(mean=[0.485, 0.456, 0.406],
                               std=[0.229, 0.224, 0.225])])
@@ -52,8 +68,6 @@ def genderAndAge(face):
     gender = genderModel(faceSq)
     _, gender = torch.max(gender, dim=1)
     gender = 'male' if gender == 0 else 'female'
-    if gender != 'male':
-        totalError += 1
 
     age = ageModel(faceSq)
     _, age = torch.max(age, dim=1)
@@ -66,3 +80,47 @@ def genderAndAge(face):
     totalFaces += 1
     totalTime += endTime - startTime
     return [gender, age]
+
+
+def gendageV2(face):
+    """
+    For now, it just returns age.
+    using regression task.
+    Args:
+        face:
+
+    Returns:
+
+    """
+    global ageModel
+    if ageModel is None:
+        ageModel = getNet('mobilenet_v3', mode='small', n_class=1)
+        ageModel.load_state_dict(
+            torch.load('D:/Gendage/output/2020-11-03_19-27-36_mobilenet_v3_regression_AgeDB_age_momentum/model.pt')
+        )
+        ageModel.eval()
+
+    global totalError, totalFaces, totalTime, ageLabels
+    transform = transforms.Compose(
+        [transforms.Resize((100, 100)),
+         transforms.ToTensor(),
+         transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                              std=[0.229, 0.224, 0.225])])
+    face = Image.fromarray(face)
+    x, y = face.size
+    size = max(x, y)
+    faceSq = Image.new('RGB', (size, size), (0, 0, 0))
+    faceSq.paste(face, (int((size - x) / 2), int((size - y) / 2)))
+    faceSq = transform(faceSq)
+    faceSq = faceSq.unsqueeze(0).to(config.device)
+
+    startTime = time.time()
+
+    age = ageModel(faceSq)
+    age = str(round(age.item(), 1))
+
+    endTime = time.time()
+
+    totalFaces += 1
+    totalTime += endTime - startTime
+    return [age]
