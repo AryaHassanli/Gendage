793b674b83072b047fc2f512a635e5f1fde257a1
Seyyed Arya Hassanli <31996976+AryaHassanli@users.noreply.github.com>
2020-11-12 09:21:07 +0100

    multi-task supported

--- train.py
+++ train.py
@@ -1,26 +1,29 @@
 import datetime
-import logging
 import os
-import sys
-import time
 
 import numpy as np
+import pytz
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
 import torchvision.transforms as transforms
-from torch.nn import MSELoss
-from torch.optim import SGD
 
 from config import config
-from helpers import parseArguments
 from helpers import getDatasetHandler
 from helpers import getNet
+from helpers import logger
+from helpers import parseArguments
 
 # Handle arguments
 args = parseArguments.parse('train')
 outputSubDir = '_'.join(
-    [datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"), args.net, args.task, args.dataset, args.feature, args.tag])
+    [datetime.datetime.now(pytz.utc).strftime("%Y-%m-%d_%H-%M-%S"), args.net, args.task, args.dataset,
+     '+'.join(args.features),
+     args.tag])
+
+# Setting up the config according to the system that runs the code
+# On Gradient the directories would be different
 if args.gradient:
     outputDir = os.path.join('/artifacts', outputSubDir)
     config.set(datasetDir='/storage/datasets',
@@ -31,165 +34,263 @@ else:
                outputDir=outputDir)
 
 # Setup Logging
-log = logging.getLogger()
-log.setLevel(logging.INFO)
-output_file_handler = logging.FileHandler(os.path.join(config.outputDir, 'output.log'))
-stdout_handler = logging.StreamHandler(sys.stdout)
-log.addHandler(output_file_handler)
-log.addHandler(stdout_handler)
+# The logs will shown on stdout and saved in $outputDir/output.log
+log = logger.Train()
 
-# set kwargs
-kwargs = {'mode': 'small'}
-if args.dataset == 'AgeDB':
-    kwargs['usePreProcessed'] = 1
+# Set the parameters for nets and datasets in kwargs to pass them to their handlers
+kwargs = {
+}
+
+# Set the number of output classes to "one" if the task is regression
 if args.task == 'regression':
+    # num_classes has been used in resnet family models and mobilenet_v2
     kwargs['num_classes'] = 1
+    # n_class has been used in mobilenet_v3 model
     kwargs['n_class'] = 1
+else:
+    kwargs['num_classes'] = 118
+    kwargs['n_class'] = 118
+    kwargs['age_classes'] = 118
 
-log.info(str(args))
-log.info(str(kwargs))
+# TODO: log the train parameters
+log.environment(args, kwargs)
 
 
 def main():
-    datasetHandler = getDatasetHandler.get(dataset=args.dataset)
-    datasetHandler.createDataset(feature=args.feature,
-                                 transform=transforms.Compose([
-                                     transforms.Resize((60, 60)),
-                                     transforms.ToTensor(),
-                                     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-                                 ])
-                                 )
-    datasetHandler.splitDataset(trainSize=args.splitSize[0], validateSize=args.splitSize[1], testSize=args.splitSize[2],
-                                **kwargs)
-    trainLoader, validateLoader, testLoader = datasetHandler.dataLoaders(batchSize=args.batchSize, **kwargs)
+    preTransforms = transforms.Compose([
+        transforms.Resize((100, 100)),
+        transforms.Pad(10),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+    runtimeTrainTransform = transforms.Compose([
+        transforms.RandomApply([
+            transforms.RandomRotation(30),
+        ], 0.7),
+        transforms.RandomPerspective(0.5, 0.5),
+        transforms.RandomHorizontalFlip(0.5)
+    ])
+    validTransform = preTransforms
 
-    numOfEpochs = args.epochs
+    log.transforms(preTransforms, runtimeTrainTransform, validTransform)
+
+    # Creating an instance of desired datasetHandler
+    datasetHandler = getDatasetHandler.get(dataset=args.dataset)
+
+    # Creating the dataset object. It will take care of Downloading and unpacking the dataset if it's not available
+    # If the dataset is preprocessed before, it can be used by setting the --usePreprocessed argument in command-line
+    # preload option, will load the whole dataset to memory on init. use it with --preload
+    datasetHandler.createDataset(features=args.features,
+                                 transform=preTransforms,
+                                 preload=args.preload,
+                                 usePreprocessed=args.usePreprocessed,
+                                 **kwargs
+                                 )
+
+    # Splitting the created dataset in train, validate, and test set.
+    trainLoader, validateLoader, testLoader = datasetHandler.getLoaders(trainSize=args.splitSize[0],
+                                                                        validateSize=args.splitSize[1],
+                                                                        testSize=args.splitSize[2],
+                                                                        batchSize=args.batchSize, **kwargs)
 
     model = getNet.get(args.net, **kwargs).to(config.device)
 
+    numOfEpochs = args.epochs
     torch.backends.cudnn.benchmark = True
-    # Mostly from https://www.kaggle.com/basu369victor/pytorch-tutorial-the-classification
     if args.task == 'classification':
-        criterion = nn.CrossEntropyLoss()
-        optimizer = optim.Adam(model.parameters(), lr=0.0001)
-
-        n_epochs = numOfEpochs
-        print_every = 10
-        valid_loss_min = np.Inf
-        val_loss = []
-        val_acc = []
-        train_loss = []
-        train_acc = []
-        total_step = len(trainLoader)
-        startTime = time.time()
-        for epoch in range(1, n_epochs + 1):
-            running_loss = 0.0
-            correct = 0
-            total = 0
-            log.info(f'Epoch {epoch}\n')
-            for batch_idx, (data_, target_) in enumerate(trainLoader):
-                data_, target_ = data_.to(config.device), target_.to(config.device)  # on GPU
-                # zero the parameter gradients
-                optimizer.zero_grad()
-                # forward + backward + optimize
-                outputs = model(data_)
-                loss = criterion(outputs, target_)
-                loss.backward()
-                optimizer.step()
-                # print statistics
-                running_loss += loss.item()
-                _, pred = torch.max(outputs, dim=1)
-                correct += torch.sum(pred == target_).item()
-                total += target_.size(0)
-                if batch_idx % 20 == 0:
-                    log.info('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
-                             .format(epoch, n_epochs, batch_idx, total_step, loss.item()))
-            train_acc.append(100 * correct / total)
-            train_loss.append(running_loss / total_step)
-            log.info(f'\ntrain loss: {np.mean(train_loss):.4f}, train acc: {(100 * correct / total):.4f}')
-            batch_loss = 0
-            total_t = 0
-            correct_t = 0
-            with torch.no_grad():
-                model.eval()
-                for data_t, target_t in validateLoader:
-                    data_t, target_t = data_t.to(config.device), target_t.to(config.device)  # on GPU
-                    outputs_t = model(data_t)
-                    loss_t = criterion(outputs_t, target_t)
-                    batch_loss += loss_t.item()
-                    _, pred_t = torch.max(outputs_t, dim=1)
-                    correct_t += torch.sum(pred_t == target_t).item()
-                    total_t += target_t.size(0)
-                val_acc.append(100 * correct_t / total_t)
-                val_loss.append(batch_loss / len(validateLoader))
-                network_learned = batch_loss < valid_loss_min
-                log.info(
-                    f'validation loss: {np.mean(val_loss):.4f}, validation acc: {(100 * correct_t / total_t):.4f}\n')
-                # Saving the best weight
-                if network_learned:
-                    valid_loss_min = batch_loss
-                    torch.save(model.state_dict(),
-                               os.path.join(config.outputDir,
-                                            args.net + '_' + args.dataset + '_' + args.feature + '_model.pt'))
-                    log.info('Detected network improvement, saving current model')
-            model.train()
-
-        batch_loss = 0
-        total_t = 0
-        correct_t = 0
-        with torch.no_grad():
-            model.eval()
-            for data_t, target_t in testLoader:
-                data_t, target_t = data_t.to(config.device), target_t.to(config.device)  # on GPU
-                outputs_t = model(data_t)
-                loss_t = criterion(outputs_t, target_t)
-                batch_loss += loss_t.item()
-                _, pred_t = torch.max(outputs_t, dim=1)
-                correct_t += torch.sum(pred_t == target_t).item()
-                total_t += target_t.size(0)
-            val_loss.append(batch_loss / len(validateLoader))
-            log.info(f'test acc: {(100 * correct_t / total_t):.4f}\n')
-        log.info("Total Time:{}s".format(time.time() - startTime))
-    elif args.task == 'regression':
-        criterion = MSELoss().to(config.device)
-        optimizer = SGD(model.parameters(), lr=0.0001, momentum=0.9)
-        valid_loss_min = np.inf
+        # https://github.com/VHCC/PyTorch-age-estimation
+        optimizer = optim.AdamW(model.parameters(), lr=0.001, weight_decay=0.001)
+        ageCriterion = nn.CrossEntropyLoss().to(config.device)
+        genderCriterion = nn.CrossEntropyLoss().to(config.device)
+        criterions = {'age': ageCriterion, 'gender': genderCriterion}
         for epoch in range(1, numOfEpochs + 1):
-            batch_loss = 0
-            for batch_idx, (data_, target_) in enumerate(trainLoader):
-                data_ = data_.to(config.device)
-                target_ = target_.to(config.device).to(torch.float32)  # on GPU
-                y_pred = model(data_)
-                loss = criterion(y_pred.squeeze(1), target_)
-                batch_loss = loss.item()
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
-                if batch_idx % 20 == 0:
-                    log.info("batch: {}, loss: {}".format(batch_idx, batch_loss))
-            log.info("Epoch: {}, loss: {}".format(epoch, batch_loss))
-            with torch.no_grad():
-                model.eval()
-                val_loss = 0
-                total_t = 0
-                for data_t, target_t in validateLoader:
-                    data_t, target_t = data_t.to(config.device), target_t.to(config.device)  # on GPU
-                    outputs_t = model(data_t)
-                    loss_t = criterion(outputs_t.squeeze(1), target_t)
-                    val_loss += loss_t.item()
-                    total_t += 1
-                val_loss /= total_t
-                network_learned = val_loss < valid_loss_min
-                log.info('validation loss: {}\n'.format(np.mean(val_loss)))
-                # Saving the best weight
-                if network_learned:
-                    valid_loss_min = val_loss
-                    torch.save(model.state_dict(),
-                               os.path.join(config.outputDir, 'model.pt'))
-                    log.info('Detected network improvement, saving current model')
-            model.train()
+            log.epochBegin(epoch, numOfEpochs)
 
+            train(model, args.features, trainLoader, criterions, optimizer, runtimeTrainTransform)
+            validate(model, args.features, validateLoader, criterions)
+
+        test(model, args.features, testLoader, criterions)
         pass
+
+
+class AverageMeter(object):
+    def __init__(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def train(model, features, trainLoader, criterions, optimizer, runtimeTrainTransform):
+    lossMonitor = {feature: AverageMeter() for feature in features}
+    accMonitor = {feature: AverageMeter() for feature in features}
+
+    model.train()
+    numOfBatches = len(trainLoader)
+    for batch, (image, labels) in enumerate(trainLoader):
+        image = image.to(config.device)
+        labels = {feature: labels[feature].to(config.device) for feature in features}
+
+        if runtimeTrainTransform is not None:
+            image = runtimeTrainTransform(image)
+
+        # compute output
+        outputs = model(image)
+        loss = {}
+        batchLoss = {}
+        predicted = {}
+        correctCount = {}
+        batchSize = image.size(0)
+        for feature in features:
+            # calc loss
+            loss[feature] = criterions[feature](outputs[feature], labels[feature])
+            batchLoss[feature] = loss[feature].item()
+
+            # calc accuracy
+            _, predicted[feature] = outputs[feature].max(1)
+            correctCount[feature] = predicted[feature].eq(labels[feature]).sum().item()
+
+            # measure accuracy and record loss
+            lossMonitor[feature].update(batchLoss[feature], batchSize)
+            accMonitor[feature].update(correctCount[feature], batchSize)
+
+        # compute gradient and do SGD step
+        loss = sum(loss.values())
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        if batch % 10 == 0:
+            batchAcc = {feature: correctCount[feature] / batchSize for feature in features}
+            log.batch(features, batch, numOfBatches, batchLoss, batchAcc)
+
+    trainLoss = {feature: lossMonitor[feature].avg for feature in features}
+    trainAcc = {feature: accMonitor[feature].avg for feature in features}
+
+    log.train(features, trainLoss, trainAcc)
+    return
+
+
+minValidateMAE = {}
+
+
+def validate(model, features, validateLoader, criterions):
+    global minValidateMAE
+    if len(minValidateMAE) == 0:
+        minValidateMAE = {feature: np.inf for feature in features}
+
+    model.eval()
+    lossMonitor = {feature: AverageMeter() for feature in features}
+    accMonitor = {feature: AverageMeter() for feature in features}
+    preds = {feature: [] for feature in features}
+    gt = {feature: [] for feature in features}
+
+    with torch.no_grad():
+        for batch, (image, labels) in enumerate(validateLoader):
+            image = image.to(config.device)
+            labels = {feature: labels[feature].to(config.device) for feature in features}
+
+            # compute output
+            outputs = model(image)
+            for feature in features:
+                preds[feature].append(F.softmax(outputs[feature], dim=-1).cpu().numpy())
+                gt[feature].append(labels[feature].cpu().numpy())
+
+            # calc loss
+            loss = {feature: criterions[feature](outputs[feature], labels[feature]) for feature in features}
+            batchLoss = {feature: loss[feature].item() for feature in features}
+
+            predicted = {}
+            correctCount = {}
+            batchSize = image.size(0)
+            for feature in features:
+                # calc accuracy
+                _, predicted[feature] = outputs[feature].max(1)
+                correctCount[feature] = predicted[feature].eq(labels[feature]).sum().item()
+
+                # measure accuracy and record loss
+                lossMonitor[feature].update(batchLoss[feature], batchSize)
+                accMonitor[feature].update(correctCount[feature], batchSize)
+
+    mae = {}
+    for feature in features:
+        preds[feature] = np.concatenate(preds[feature], axis=0)
+        classes = np.arange(0, preds[feature].shape[1])
+        gt[feature] = np.concatenate(gt[feature], axis=0)
+        ave_preds = (preds[feature] * classes).sum(axis=-1)
+        diff = ave_preds - gt[feature]
+        mae[feature] = np.abs(diff).mean()
+
+    validateLoss = {feature: lossMonitor[feature].avg for feature in features}
+    validateAcc = {feature: accMonitor[feature].avg for feature in features}
+    validateMAE = {feature: mae[feature] for feature in features}
+
+    isLearned = {feature: validateMAE[feature] < minValidateMAE[feature] for feature in features}
+    # TODO
+    isLearned = sum(isLearned.values())
+    if isLearned:
+        minValidateMAE = {feature: validateMAE[feature] for feature in features}
+        torch.save(model.state_dict(),
+                   os.path.join(config.outputDir, 'model.pt'))
+
+    log.validate(features, validateLoss, validateAcc, validateMAE, isLearned)
+    return
+
+
+def test(model, features, testLoader, criterions):
+    model.eval()
+    lossMonitor = {feature: AverageMeter() for feature in features}
+    accMonitor = {feature: AverageMeter() for feature in features}
+    preds = {feature: [] for feature in features}
+    gt = {feature: [] for feature in features}
+
+    with torch.no_grad():
+        for batch, (image, labels) in enumerate(testLoader):
+            image = image.to(config.device)
+            labels = {feature: labels[feature].to(config.device) for feature in features}
+
+            # compute output
+            outputs = model(image)
+            for feature in features:
+                preds[feature].append(F.softmax(outputs[feature], dim=-1).cpu().numpy())
+                gt[feature].append(labels[feature].cpu().numpy())
+
+            # calc loss
+            loss = {feature: criterions[feature](outputs[feature], labels[feature]) for feature in features}
+            batchLoss = {feature: loss[feature].item() for feature in features}
+
+            predicted = {}
+            correctCount = {}
+            batchSize = image.size(0)
+            for feature in features:
+                # calc accuracy
+                _, predicted[feature] = outputs[feature].max(1)
+                correctCount[feature] = predicted[feature].eq(labels[feature]).sum().item()
+
+                # measure accuracy and record loss
+                lossMonitor[feature].update(batchLoss[feature], batchSize)
+                accMonitor[feature].update(correctCount[feature], batchSize)
+
+    mae = {}
+    for feature in features:
+        preds[feature] = np.concatenate(preds[feature], axis=0)
+        classes = np.arange(0, preds[feature].shape[1])
+        gt[feature] = np.concatenate(gt[feature], axis=0)
+        ave_preds = (preds[feature] * classes).sum(axis=-1)
+        diff = ave_preds - gt[feature]
+        mae[feature] = np.abs(diff).mean()
+
+    testLoss = {feature: lossMonitor[feature].avg for feature in features}
+    testAcc = {feature: accMonitor[feature].avg for feature in features}
+    testMAE = {feature: mae[feature] for feature in features}
+
+    log.test(features, testLoss, testAcc, testMAE)
+    return
 
 
 if __name__ == '__main__':
