9dca07195fbc85acf6df9b4be597ef3b32657ab0
Seyyed Arya Hassanli <31996976+AryaHassanli@users.noreply.github.com>
2020-11-13 16:22:34 +0100

    no need for baseDir

--- dataLoaders/__init__.py
+++ dataLoaders/__init__.py
@@ -2,6 +2,6 @@ import os
 
 from config import config
 
-dataLoadersDir = os.path.join(config.baseDir, 'dataLoaders')
+dataLoadersDir = 'dataLoaders'
 __all__ = [f[:-3] for f in os.listdir(dataLoadersDir) if
            os.path.isfile(os.path.join(dataLoadersDir, f)) and not f.endswith('__init__.py')]
--- nets/__init__.py
+++ nets/__init__.py
@@ -2,6 +2,6 @@ import os
 
 from config import config
 
-netsDir = os.path.join(config.baseDir, 'nets')
+netsDir = 'nets'
 __all__ = [f[:-3] for f in os.listdir(netsDir) if
            os.path.isfile(os.path.join(netsDir, f)) and not f.endswith('__init__.py')]
