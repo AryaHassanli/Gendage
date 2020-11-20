6062229d2db930c1074fdd3f43bf24ee5c69ee23
Seyyed Arya Hassanli <31996976+AryaHassanli@users.noreply.github.com>
2020-11-20 11:16:09 +0100

    File reformatted

--- nets/__init__.py
+++ nets/__init__.py
@@ -1,7 +1,5 @@
 import os
 
-from config import config
-
 netsDir = 'nets'
 __all__ = [f[:-3] for f in os.listdir(netsDir) if
-           os.path.isfile(os.path.join(netsDir, f)) and not f.endswith('__init__.py')]
+           os.path.isfile(os.path.join(netsDir, f)) and not f.endswith(('__init__.py', '__pycache__'))]
