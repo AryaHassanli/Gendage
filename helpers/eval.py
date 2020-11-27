0822bbea3cf63d3562a641b5ecf835bc17de4089
Seyyed Arya Hassanli <31996976+AryaHassanli@users.noreply.github.com>
2020-11-27 12:50:01 +0100

    modified according to encoder+classifier model

--- helpers/eval.py
+++ helpers/eval.py
@@ -2,29 +2,30 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 import torchvision.transforms as transforms
+from facenet_pytorch import InceptionResnetV1
 
 from config import config
 
+resnet = InceptionResnetV1(pretrained='vggface2').eval().to(config.device)
 
-def faceImage(face, model, features):
-    model.eval()
+
+def encoderMultiTask(face, ageModel, genderModel):
+    ageModel.eval()
+    genderModel.eval()
     with torch.no_grad():
-        preTransforms = transforms.Compose([
-            transforms.Resize((100, 100)),
-            transforms.Pad(10),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-        ])
+        preTransforms = config.preTransforms
+        face = preTransforms(face).to(config.device)
+        face = face.unsqueeze(0)
+        face = resnet(face).detach()
 
-        face = preTransforms(face)
-        face = face.unsqueeze(0).to(config.device)
-        outputs = model(face)
+        ageOutputs = ageModel(face)
+        genderOutputs = genderModel(face)
 
-        preds = {feature: [] for feature in features}
-        ave_preds = {feature: 0 for feature in features}
-        for feature in features:
-            preds[feature].append(F.softmax(outputs[feature], dim=-1).cpu().numpy())
-            preds[feature] = np.concatenate(preds[feature], axis=0)
-            classes = np.arange(0, preds[feature].shape[1])
-            ave_preds[feature] = (preds[feature] * classes).sum(axis=-1)[0]
-    return ave_preds
+        preds = F.softmax(ageOutputs, dim=-1).cpu().numpy()
+        classes = np.arange(0, preds.shape[1])
+        agePred = (preds * classes).sum(axis=-1)[0]
+
+        preds = F.softmax(genderOutputs, dim=-1).cpu().numpy()
+        classes = np.arange(0, preds.shape[1])
+        genderPred = (preds * classes).sum(axis=-1)[0]
+    return {"age": agePred, "gender": genderPred}
