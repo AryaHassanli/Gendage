62430095bbc91aa5780217676f431a2fb840d16d
AryaHassanli <a.hasanli@yahoo.com>
2020-12-09 21:51:06 +0100

    getClassifier added instead of getNet

---
+++ src/helpers/getClassifier.py
@@ -0,0 +1,14 @@
+import sys
+# noinspection PyUnresolvedReferences
+from models.classifier import *
+
+
+def get(classifier_name, **kwargs):
+    classifier = getattr(eval(classifier_name), classifier_name)
+    if callable(classifier):
+        print(classifier_name + " Found!")
+        classifier = classifier(**kwargs)
+    else:
+        sys.exit('classifier not found')
+
+    return classifier
