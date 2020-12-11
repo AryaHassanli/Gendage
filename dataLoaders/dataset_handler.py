c8a969a630893133b8f3381b469343fa229b3984
AryaHassanli <a.hasanli@yahoo.com>
2020-12-11 11:24:12 +0100

    Added support for test

--- dataLoaders/dataset_handler.py
+++ dataLoaders/dataset_handler.py
@@ -10,16 +10,18 @@ class DatasetHandler:
         pass
 
     def get_loaders(self, train_size=0.7, validate_size=0.2, test_size=0.1, batch_size=15, **kwargs):
-        if round(train_size + validate_size + test_size, 1) != 1.0:
-            sys.exit("Sum of the percentages should be equal to 1. it's " + str(
+        if round(train_size + validate_size + test_size, 1) > 1.0:
+            sys.exit("Sum of the percentages should be less than 1. it's " + str(
                 train_size + validate_size + test_size) + " now!")
 
         train_len = int(len(self.dataset) * train_size)
         validate_len = int(len(self.dataset) * validate_size)
-        test_len = len(self.dataset) - train_len - validate_len
+        # test_len = len(self.dataset) - train_len - validate_len
+        test_len = int(len(self.dataset) * test_size)
+        others_len = len(self.dataset) - train_len - validate_len - test_len
 
-        self.trainDataset, self.validateDataset, self.testDataset = torch.utils.data.random_split(
-            self.dataset, [train_len, validate_len, test_len])
+        self.trainDataset, self.validateDataset, self.testDataset, _ = torch.utils.data.random_split(
+            self.dataset, [train_len, validate_len, test_len, others_len])
 
         self.dataset = None
 
